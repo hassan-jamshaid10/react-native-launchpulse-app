72d8428970b6764a4bdbfede418730400fd63c5f
hassan-jamshaid10 <hjamshaid81@gmail.com>
2026-04-04 20:18:06 +0500

    feat: complete mobile Auth UI and TRPC superjson integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-gesture-handler';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { queryClient, trpc, trpcClient } from '@/utils/api';
 
 export const unstable_settings = {
   initialRouteName: 'index',
@@ -14,14 +16,18 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="auth" options={{ headerShown: false }} />
-        <Stack.Screen name="dashboard" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="auth" options={{ headerShown: false }} />
+            <Stack.Screen name="dashboard" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+          </Stack>
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </trpc.Provider>
   );
 }
--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,16 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+const { resolver } = config;
+
+// @tanstack/react-query v5 support
+config.resolver.sourceExts.push('cjs');
+config.resolver.sourceExts.push('mjs');
+
+// 1. Enable package exports
+config.resolver.unstable_enablePackageExports = true;
+
+// 2. Add condition names to ensure we pick up the right entry point
+config.resolver.unstable_conditionNames = ['react-native', 'browser', 'require', 'import', 'default'];
+
 module.exports = config;
